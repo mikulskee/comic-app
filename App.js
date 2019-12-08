4cab4309434e4a168050873560d0fbb897726ad6
Mateusz Mikulski <mikulskee@gmail.com>
2019-12-08 10:49:02 +0100

    added react-navigation

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity
+} from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import Header from "./components/Header";
 import Card from "./components/Card";
 
-export default function App() {
+const HomeScreen = ({ navigation }) => {
   const [latestNum, setLatestNum] = useState();
   const [data, setData] = useState([]);
 
@@ -52,24 +60,65 @@ export default function App() {
 
       <ScrollView style={styles.content}>
         {data.map(item => (
-          <Card key={item.title} item={item} />
+          <TouchableOpacity
+            key={item.title}
+            onPress={() => navigation.navigate("Details", item)}
+          >
+            <Card item={item} />
+          </TouchableOpacity>
         ))}
       </ScrollView>
     </View>
   );
-}
+};
+
+const DetailsScreen = ({ navigation }) => {
+  const rating = navigation.state.params;
+  return (
+    <View style={styles.container}>
+      <Image
+        resizeMode="contain"
+        style={styles.img}
+        source={{ uri: `${rating.img}` }}
+      />
+      {/* <View style={styles.img} /> */}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 20,
     backgroundColor: "#121212",
-    paddingHorizontal: 20
+    paddingHorizontal: 5
+    // justifyContent: "center",
+    // alignItems: "center"
   },
   title: {
     color: "#fff"
   },
   content: {
     marginTop: 20
+  },
+
+  img: {
+    flex: 1,
+    resizeMode: "contain"
   }
 });
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default function App() {
+  return <AppContainer />;
+}
